3870a8988bfc05cc17d03f8776266063208e1403
Richard King <richard@openrightsgroup.org>
2015-04-18 16:27:05 +0200

    Refactor front end
    - Add an app for voters
    - Move homepage content into voters app
    - Add functional and unit tests for homepage

--- q_and_a/urls.py
+++ q_and_a/urls.py
@@ -10,11 +10,10 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
+    url(r'^$', 'voters.views.HomePageView', name='home'),
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
     url(r'^organisations', include('organisations.urls')),
     url(r'^candidates', include('candidates.urls')),
     url(r'^api/v1/question/(\d+)/?$', 'questions.views.api'),
-    url(r'^$', TemplateView.as_view(template_name="home.html"), name='home'),
-    # url(r'^', include('prototype.urls')),
 ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
