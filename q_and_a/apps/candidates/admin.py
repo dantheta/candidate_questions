d8d801b3d39607d961a01ca7bb8fc089a10a8954
Edward Betts <edward@4angle.com>
2015-04-22 15:14:14 +0100

    Allow searching candidates by e-mail address

--- q_and_a/apps/candidates/admin.py
+++ q_and_a/apps/candidates/admin.py
@@ -4,6 +4,6 @@ from .models import Candidate
 class CandidateAdmin(admin.ModelAdmin):
     list_display = ('name', 'contact_address', 'constituency_name', 'party')
     list_filter = ('party', 'participating')
-    search_fields = ('name', 'constituency_name', 'party')
+    search_fields = ('name', 'constituency_name', 'party', 'contact_address')
 
 admin.site.register(Candidate, CandidateAdmin)
