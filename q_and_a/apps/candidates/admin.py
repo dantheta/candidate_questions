49f3b9751361ccf4ae0ab11d17277c4d4379d31c
Edward Betts <edward@4angle.com>
2015-04-22 14:07:04 +0100

    Admin filter candidates by participating flag.

--- q_and_a/apps/candidates/admin.py
+++ q_and_a/apps/candidates/admin.py
@@ -3,7 +3,7 @@ from .models import Candidate
 
 class CandidateAdmin(admin.ModelAdmin):
     list_display = ('name', 'contact_address', 'constituency_name', 'party')
-    list_filter = ('party', )
+    list_filter = ('party', 'participating')
     search_fields = ('name', 'constituency_name', 'party')
 
 admin.site.register(Candidate, CandidateAdmin)
